5ef17f0662ad88f83dbbe249d3cedf3d2601285c
stein17 <lutz.f.kroll@gmail.com>
2020-09-10 10:09:07 +0200

    add new update AX_Blue_FHD_4ATV

---
+++ AX-Blue-FHD-4ATV/usr/lib/enigma2/python/Plugins/Extensions/AXBlueFHD/__init__.py
@@ -0,0 +1,25 @@
+from __future__ import print_function
+# Embedded file name: /usr/lib/enigma2/python/Plugins/Extensions/AXBlueConfig/__init__.py
+from Components.Language import language
+from Tools.Directories import resolveFilename, SCOPE_PLUGINS
+import os, gettext
+PluginLanguageDomain = 'AXBlueConfig'
+PluginLanguagePath = 'Extensions/AXBlueConfig/locale'
+
+def localeInit():
+    lang = language.getLanguage()[:2]
+    os.environ['LANGUAGE'] = lang
+    print('[WebInterface] set language to ', lang)
+    gettext.bindtextdomain(PluginLanguageDomain, resolveFilename(SCOPE_PLUGINS, PluginLanguagePath))
+
+
+def _(txt):
+    t = gettext.dgettext(PluginLanguageDomain, txt)
+    if t == txt:
+        print('[%s] fallback to default translation for %s' % (PluginLanguageDomain, txt))
+        t = gettext.gettext(txt)
+    return t
+
+
+localeInit()
+language.addCallback(localeInit)
